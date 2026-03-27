6d6c3cb1de064134a214dd32ca10b65c8d4f2f03
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-03-27 17:05:29 +0700

    UserService

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './models/user.entity';
 
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get()
-  all() {
-    return ['users'];
+  all(): Promise<User[]> {
+    return this.userService.all();
   }
 }
