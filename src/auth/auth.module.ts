507881c1e8ef92000440ca2c04f51c89a8f35d91
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-04-08 13:11:29 +0700

    creating user endpoint

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
-import { JwtModule } from '@nestjs/jwt';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
-  imports: [
-    UserModule,
-    JwtModule.register({
-      global: true,
-      secret: 'secret',
-      signOptions: { expiresIn: '1d' },
-    }),
-  ],
+  imports: [UserModule, CommonModule],
   controllers: [AuthController],
 })
 export class AuthModule {}
