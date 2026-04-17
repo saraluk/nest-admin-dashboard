8be4a0020b153920273b685181bcbe5921a433fd
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-04-17 13:35:36 +0700

    products

---
+++ src/product/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from 'src/common/abstract.service';
+import { Product } from './models/product.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProductService extends AbstractService {
+  constructor(
+    @InjectRepository(Product) private productRepository: Repository<Product>,
+  ) {
+    super(productRepository);
+  }
+}
