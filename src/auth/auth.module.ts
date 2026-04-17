0d7f6c5831a5cb4b16a0df9c8a38a8ed8dbc492f
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-04-17 13:18:46 +0700

    endpoints to update user info and password

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { CommonModule } from 'src/common/common.module';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UserModule, CommonModule],
+  imports: [forwardRef(() => UserModule), CommonModule],
   controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
