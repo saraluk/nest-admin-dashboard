8fbea6e4935f6993c3ff80689f8db41fddda3e78
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-04-19 14:31:51 +0700

    raw SQL query

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -71,4 +71,9 @@ export class OrderController {
     res.attachment('orders.csv');
     return res.send(csv);
   }
+
+  @Get('chart')
+  async chart() {
+    return this.orderService.chart();
+  }
 }
